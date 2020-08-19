b901752fdcb28af05920e257d69317a8136a1574
parsa <parsak550z@gmail.com>
2020-08-19 13:20:33 +0430

    deleted the unused cupertiono packages

--- lib/Screens/loading_screen.dart
+++ lib/Screens/loading_screen.dart
@@ -1,6 +1,5 @@
 import 'package:arena/Screens/ShopScreen/ItemsScreen.dart';
 import 'package:arena/services/networking.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 
