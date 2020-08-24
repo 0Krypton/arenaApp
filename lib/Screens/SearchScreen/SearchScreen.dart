d3149e7ce7d42e28e95023d131b7ea62df10fd52
krypt0_n <asoheil820614@gmail.com>
2020-08-24 13:55:34 +0430

    added some images+completed SearchScreenItemDetail

---
+++ lib/Screens/SearchScreen/SearchScreen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class SearchScreen extends StatefulWidget {
+  @override
+  _SearchScreenState createState() => _SearchScreenState();
+}
+
+class _SearchScreenState extends State<SearchScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/Utilities/SearchScreenItemDetail.dart
@@ -0,0 +1,115 @@
+import 'package:flutter/material.dart';
+
+class SearchScreenTournoumentDetail {
+  final String creator;
+  final String title;
+  final String description;
+  final double prize;
+  final bool isPublic;
+  final Color bgColor;
+  final String imageUrl;
+  final int totalPlayers;
+  final int enteredPlayers;
+
+  SearchScreenTournoumentDetail({
+    this.creator,
+    this.title,
+    this.description,
+    this.prize,
+    this.isPublic,
+    this.bgColor,
+    this.imageUrl,
+    this.totalPlayers,
+    this.enteredPlayers,
+  });
+}
+
+List<SearchScreenTournoumentDetail> searchScreenTournouments = [
+  SearchScreenTournoumentDetail(
+    creator: 'User1',
+    isPublic: false,
+    title: 'Pro FNCS ZoneWars',
+    description: 'Fortnite 16v16 Fncs ZoneWars\nwith Prizes',
+    totalPlayers: 16,
+    enteredPlayers: 9,
+    prize: 9.99,
+    imageUrl: 'images/forntite_regade_raider_cropped.png',
+    bgColor: Color(0xFF8A3528),
+  ),
+  SearchScreenTournoumentDetail(
+    creator: 'User2',
+    isPublic: false,
+    title: 'Custom Tournament',
+    description: 'First Pro Scrim with Great\nPrizes',
+    totalPlayers: 8,
+    enteredPlayers: 3,
+    prize: 19.99,
+    imageUrl: 'images/Reyna_Valorant_cropped.png',
+    bgColor: Color(0xFF1F173C), 
+  ),
+  SearchScreenTournoumentDetail(
+    creator: 'User3',
+    isPublic: true,
+    title: 'ANCS Tournament Open',
+    description: 'The beginning of ANCS\nTournament now Available!',
+    enteredPlayers: 254,
+    prize: 50,
+    imageUrl: 'images/fortnite-seeker_cropped.png',
+    bgColor: Color(0xFF519BB1), 
+  ),
+  SearchScreenTournoumentDetail(
+    creator: 'User4',
+    isPublic: false,
+    title: 'Public Box PvP',
+    description: 'Win the all two Round\nto get your prize',
+    totalPlayers: 16,
+    enteredPlayers: 4,
+    prize: 5,
+    imageUrl: 'images/rabbit_raider_cropped.png',
+    bgColor: Color(0xFFE568A5), 
+  ),
+  SearchScreenTournoumentDetail(
+    creator: 'User5',
+    isPublic: false,
+    title: 'Pro Creative Tournament',
+    description: 'Best & hardship Battle for\nAll of you, so good luck :)',
+    totalPlayers: 256,
+    enteredPlayers: 93,
+    prize: 50,
+    imageUrl: 'images/fortnite_ghoul_trooper_cropped.png',
+    bgColor: Color(0xFF75B0A6), 
+  ),
+  SearchScreenTournoumentDetail(
+    creator: 'User6',
+    isPublic: false,
+    title: 'Pro Arena Scrims',
+    description: 'Play with best & pro players\nin the This Pro Scrim',
+    totalPlayers: 100,
+    enteredPlayers: 43,
+    prize: 20,
+    imageUrl: 'images/fortnite_torunoment_image_3.png',
+    bgColor: Color(0xFF6A02F7), 
+  ),   
+  SearchScreenTournoumentDetail(
+    creator: 'User7',
+    isPublic: true,
+    title: 'Boxfight & Zonewars',
+    description: 'Play with best & pro players\nin the This Pro Scrim',
+    totalPlayers: 16,
+    enteredPlayers: 3,
+    prize: 1.99,
+    imageUrl: 'images/fortnite_starFlare_cropped.png',
+    bgColor: Color(0xFF9E0D28), 
+  ),   
+  SearchScreenTournoumentDetail(
+    creator: 'User8',
+    isPublic: true,
+    title: 'Open Tournament',
+    description: 'The public tournoument with\nAwesome Prize',
+    totalPlayers: 8,
+    enteredPlayers: 5,
+    prize: 14.99,
+    imageUrl: 'images/Phoenix_valorant_cropped.png',
+    bgColor: Color(0xFFFACC56), 
+  ),
+];
